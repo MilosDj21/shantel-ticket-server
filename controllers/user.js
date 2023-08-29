89a62f1e32379c9cc20015e8d8962c1d01b200a5
Milos <m.djuka@hotmail.com>
2023-08-29 16:20:57 +0200

    small changes

--- controllers/user.js
+++ controllers/user.js
@@ -5,13 +5,16 @@ const User = require("../models/User");
 module.exports.saveOne = async (req, res) => {
   const { email, firstName, lastName, password, roles } = req.body;
   const profileImage = req.file;
+  // console.log(profileImage);
   try {
     const user = await User.signup(email, password, firstName, lastName, roles, profileImage);
     res.status(200).json({ status: "success", data: user });
   } catch (err) {
-    fs.unlink(path.join(__dirname, `../${profileImage.path}`), (error) => {
-      if (error) console.log(error);
-    });
+    if (profileImage) {
+      fs.unlink(path.join(__dirname, `../${profileImage.path}`), (error) => {
+        if (error) console.log(error);
+      });
+    }
     res.status(400).json({ status: "failed", message: err.message });
   }
 };
