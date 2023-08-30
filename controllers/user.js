33c64afc39b0d6e4877e08ddffa0a915f902051d
Milos <m.djuka@hotmail.com>
2023-08-30 16:36:12 +0200

    added route and controller for fetching all users

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const User = require("../models/User");
+const Role = require("../models/Role");
 
 module.exports.saveOne = async (req, res) => {
   const { email, firstName, lastName, password, roles } = req.body;
@@ -8,7 +9,7 @@ module.exports.saveOne = async (req, res) => {
   // console.log(profileImage);
   try {
     const user = await User.signup(email, password, firstName, lastName, roles, profileImage);
-    res.status(200).json({ status: "success", data: user });
+    res.status(200).json({ status: "success", data: user, message: "Added Successfully!" });
   } catch (err) {
     if (profileImage) {
       fs.unlink(path.join(__dirname, `../${profileImage.path}`), (error) => {
@@ -16,5 +17,33 @@ module.exports.saveOne = async (req, res) => {
       });
     }
     res.status(400).json({ status: "failed", message: err.message });
+  }
+};
+
+module.exports.findAll = async (req, res) => {
+  try {
+    const users = await User.find({}, { password: 0 });
+    const roles = await Role.find();
+    const filteredUsers = users.map((u) => {
+      const userRoles = [];
+      for (ur of u.roles) {
+        for (r of roles) {
+          if (r._id.toString() === ur) {
+            userRoles.push(r.name);
+          }
+        }
+      }
+      return {
+        _id: u._id,
+        email: u.email,
+        firstName: u.firstName,
+        lastName: u.lastName,
+        roles: userRoles,
+        profileImage: u.profileImage,
+      };
+    });
+    res.status(200).json({ status: "status", data: filteredUsers });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
   }
 };
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { saveOne } = require("../controllers/user");
+const { saveOne, findAll } = require("../controllers/user");
 const { verifyToken, isAdmin } = require("../middlewares/auth");
 const multerConf = require("../middlewares/multerConf");
 
@@ -10,5 +10,6 @@ router.use(isAdmin);
 router.use(multerConf);
 
 router.post("/", saveOne);
+router.get("/", findAll);
 
 module.exports = router;
