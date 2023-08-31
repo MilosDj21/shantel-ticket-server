d7f08d473f5b93e8dd4b2a46f6c1d119d37ba610
Milos <m.djuka@hotmail.com>
2023-08-31 16:32:12 +0200

    added controller and route for finding single role

--- controllers/role.js
+++ controllers/role.js
@@ -1,6 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 const Role = require("../models/Role");
+const mongoose = require("mongoose");
+
+module.exports.findOne = async (req, res) => {
+  const roleId = req.params.roleId;
+  try {
+    if (!roleId || !mongoose.Types.ObjectId.isValid(roleId)) throw Error("Invalid role id");
+    const role = await Role.findById(roleId);
+    if (!role) throw Error("No such role");
+    res.status(200).json({ status: "success", data: role });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
 
 module.exports.findAll = async (req, res) => {
   try {
--- routes/role.js
+++ routes/role.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
-const { findAll } = require("../controllers/role");
+const { findAll, findOne } = require("../controllers/role");
 const { verifyToken, isAdmin } = require("../middlewares/auth");
 
 const router = Router();
 
 router.use(verifyToken);
-router.use(isAdmin);
+// router.use(isAdmin);
 
+router.get("/:roleId", findOne);
 router.get("/", findAll);
 
 module.exports = router;
