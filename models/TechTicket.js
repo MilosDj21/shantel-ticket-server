acc30348c2d11b6b35f9b7deeebc592a64983527
Milos <m.djuka@hotmail.com>
2023-09-01 11:58:38 +0200

    added model

---
+++ models/TechTicket.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const techTicketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const TechTicket = mongoose.model("TechTicket", techTicketSchema);
+
+module.exports = TechTicket;
