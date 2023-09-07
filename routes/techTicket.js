51f7db099a53df378fe1441cef87012534e95b68
Milos <m.djuka@hotmail.com>
2023-09-07 15:14:58 +0200

    refactor ticket message routes

--- routes/techTicket.js
+++ routes/techTicket.js
@@ -3,17 +3,19 @@ const { findOne, findAll, saveOne, updateOne, deleteOne, findOneByUser, findAllB
 const { verifyToken, isAdmin } = require("../middlewares/auth");
 const ticketMessageRouter = require("./techTicketMessage");
 
-// routes for all tickets, only admin can access them directly
 const router = Router();
 
 router.use(verifyToken);
-router.use(isAdmin);
 
-router.get("/:ticketId", findOne);
-router.get("/", findAll);
-router.post("/", saveOne);
-router.patch("/", updateOne);
-router.delete("/:ticketId", deleteOne);
+// routes for all tickets, only admin can access them directly
+router.get("/:ticketId", [isAdmin], findOne);
+router.get("/", [isAdmin], findAll);
+router.post("/", [isAdmin], saveOne);
+router.patch("/", [isAdmin], updateOne);
+router.delete("/:ticketId", [isAdmin], deleteOne);
+
+//routes for ticket messages, they are accessed from ticket, so user don't need to be checked
+router.use("/:ticketId/ticketMessage", ticketMessageRouter);
 
 // routes for user tickets
 const userTicketRouter = Router({ mergeParams: true });
@@ -21,9 +23,6 @@ const userTicketRouter = Router({ mergeParams: true });
 userTicketRouter.get("/:ticketId", findOneByUser);
 userTicketRouter.get("/", findAllByUser);
 userTicketRouter.post("/", saveOneByUser);
-
-//routes for ticket messages
-userTicketRouter.use("/:ticketId/ticketMessage", ticketMessageRouter);
 
 const ticketRouter = (module.exports = router);
 ticketRouter.userTicketRouter = userTicketRouter;
