80d3a229b05e0c3ee002de41ff6673a57989ef75
Milos <m.djuka@hotmail.com>
2023-11-23 15:47:28 +0100

    added models for bonus task, and bonus task message

---
+++ models/BonusTask.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const bonusTasksSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const BonusTask = mongoose.model("BonusTask", bonusTasksSchema);
+
+module.exports = BonusTask;
