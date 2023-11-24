2744df90cd2eeb4ebdeab89a916498ad0dd93658
Milos <m.djuka@hotmail.com>
2023-11-24 13:14:26 +0100

    added routes for bonus task and bonus task message

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const { saveOne, updateOne, findAll, findOne } = require("../controllers/user");
 const { verifyToken, isAdmin } = require("../middlewares/auth");
 const multerConf = require("../middlewares/multerConf");
 const { userTicketRouter } = require("./techTicket");
+const { userBonusTaskRouter } = require("./bonusTask");
 
 const router = Router();
 
@@ -16,5 +17,8 @@ router.patch("/", [isAdmin, multerConf], updateOne);
 
 // routes for tickets from a single user, handled in techTicket routes
 router.use("/:userId/techTickets", userTicketRouter);
+
+// routes for bonus tasks from a single user, handled in bonusTasks routes
+router.use("/:userId/bonusTasks", userBonusTaskRouter);
 
 module.exports = router;
