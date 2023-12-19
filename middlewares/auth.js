45a786919bcd300a2501e9fc21e2dfad73741b0c
Milos <m.djuka@hotmail.com>
2023-12-19 14:45:58 +0100

    return correct response status code when token is present, but user does not exist

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const Role = require("../models/Role");
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
@@ -13,18 +13,28 @@ const verifyToken = (req, res, next) => {
       } else {
         // console.log(decodedToken);
         req.userId = decodedToken.id;
-        next();
       }
     });
+    try {
+      const user = await User.findById(req.userId);
+      if (!user) {
+        res.status(401).json({ message: "Invalid User" });
+      } else {
+        next();
+      }
+    } catch (error) {
+      res.status(401).json({ message: "Invalid User" });
+      console.log(error);
+    }
   } else {
-    res.status(401).json({ message: "Missing token" });
+    res.status(401).json({ message: "Please Login To Continue" });
   }
 };
 
 const isAdmin = async (req, res, next) => {
   try {
     const user = await User.findById(req.userId);
-    if (!user) throw Error("Unauthorized");
+    if (!user) throw Error("Invalid User");
 
     const adminRoles = await Role.find().or([{ name: "Admin" }, { name: "Super Admin" }]);
     if (user.roles.includes(adminRoles[0]._id.toString()) || user.roles.includes(adminRoles[1]._id.toString())) {
