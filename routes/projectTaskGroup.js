79c12d26a2bd29a9145e81643716e9fff399e170
Milos <m.djuka@hotmail.com>
2024-01-17 12:30:45 +0100

    add project task group routes

---
+++ routes/projectTaskGroup.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const { findOne, findAll, saveOne, updateOne, deleteOne } = require("../controllers/projectTaskGroup");
+
+const router = Router({ mergeParams: true });
+
+router.get("/:taskGroupId", findOne);
+router.get("/", findAll);
+router.post("/", saveOne);
+router.patch("/:taskGroupId", updateOne);
+router.delete("/:taskGroupId", deleteOne);
+
+module.exports = router;
