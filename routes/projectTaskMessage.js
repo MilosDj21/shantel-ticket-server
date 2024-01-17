889ef48f6fc5f58f0cbe76a6ef3e30a22b08aadf
Milos <m.djuka@hotmail.com>
2024-01-17 12:28:35 +0100

    add project task message routes

--- routes/projectTaskMessage.js
+++ routes/projectTaskMessage.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { findOne, findAll, saveOne, updateOne, deleteOne } = require("../controllers/bonusTaskMessage");
+const { findOne, findAll, saveOne, updateOne, deleteOne } = require("../controllers/projectTaskMessage");
 const multerConf = require("../middlewares/multerConf");
 
 const router = Router({ mergeParams: true });
