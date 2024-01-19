d5047dfb5d0e8f5ff8f84f68064c64ba24ee856f
Milos <m.djuka@hotmail.com>
2024-01-19 15:56:13 +0100

    add admin find one controller

--- controllers/project/projectTask.js
+++ controllers/project/projectTask.js
@@ -1,10 +1,98 @@
+const ProjectTask = require("../../models/project/ProjectTask");
+const mongoose = require("mongoose");
+
+// only admin can access these controllers
 module.exports.adminFindOne = async (req, res) => {
-  res.status(200).json({ message: "admin find one" });
+  const { taskId } = req.params;
+  try {
+    if (!taskId || mongoose.Types.ObjectId.isValid(taskId)) throw Error("Invalid task id");
+    const tasks = await ProjectTask.aggregate([
+      { $match: { _id: new mongoose.Types.ObjectId(taskId) } },
+      { $limit: 1 },
+      {
+        $lookup: {
+          from: "users",
+          localField: "assignedUsers",
+          foreignField: "_id",
+          as: "assignedUsers",
+        },
+      },
+      {
+        $lookup: {
+          from: "projettaskmessages",
+          localField: "_id",
+          foreignField: "task",
+          as: "messages",
+          pipeline: [
+            {
+              $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "user",
+              },
+            },
+            {
+              $unwind: "$user",
+            },
+          ],
+        },
+      },
+      {
+        $lookup: {
+          from: "projects",
+          localField: "project",
+          foreignField: "_id",
+          as: "project",
+          pipeline: [
+            {
+              $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "user",
+              },
+            },
+            {
+              $unwind: "$user",
+            },
+          ],
+        },
+      },
+      {
+        $unwind: "$project",
+      },
+      {
+        $lookup: {
+          from: "projectgroups",
+          localField: "group",
+          foreignField: "_id",
+          as: "group",
+        },
+      },
+      {
+        $unwind: "$group",
+      },
+      {
+        $project: {
+          "assignedUsers.password": 0,
+          "messages.user.password": 0,
+          "project.user.password": 0,
+        },
+      },
+    ]);
+    if (!tasks) throw Error("No such task");
+    res.status(200).json({ status: "success", data: tasks[0] });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
 };
 module.exports.adminFindAll = async (req, res) => {};
 module.exports.adminSaveOne = async (req, res) => {};
 module.exports.adminUpdateOne = async (req, res) => {};
 module.exports.adminDeleteOne = async (req, res) => {};
+
+// controllers for regular users, for task view
 module.exports.taskViewFindOne = async (req, res) => {
   res.status(200).json({ message: "all good task view find one" });
 };
@@ -12,6 +100,8 @@ module.exports.taskViewFindAll = async (req, res) => {};
 module.exports.taskViewSaveOne = async (req, res) => {};
 module.exports.taskViewUpdateOne = async (req, res) => {};
 module.exports.taskViewDeleteOne = async (req, res) => {};
+
+// controllers for regular users, for project view
 module.exports.projectViewFindOne = async (req, res) => {
   res.status(200).json({ message: "all good project view find one" });
 };
