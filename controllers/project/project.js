9369b2dcf8a55468b88204ead411bdd299f7c796
Milos <m.djuka@hotmail.com>
2024-01-22 13:27:40 +0100

    split controllers into controllers + services

--- controllers/project/project.js
+++ controllers/project/project.js
@@ -1,115 +1,30 @@
-const Project = require("../../models/project/Project");
-const mongoose = require("mongoose");
+const { adminFindOne, adminFindAll, adminUpdateOne, adminDeleteOne, userFindOne, userFindAll, userSaveOne, userUpdateOne, userDeleteOne } = require("../../services/project/project");
 
-//only admin can access these controllers
-module.exports.adminFindOne = async (req, res) => {
+module.exports.findOne = async (req, res) => {
+  const { userId, userIsAdmin } = req;
   const { projectId } = req.params;
+  let project;
   try {
-    if (!projectId || !mongoose.Types.ObjectId.isValid(projectId)) throw Error("Invalid project id");
-    const projects = await Project.aggregate([
-      { $match: { _id: new mongoose.Types.ObjectId(projectId) } },
-      { $limit: 1 },
-      {
-        $lookup: {
-          from: "users",
-          localField: "user",
-          foreignField: "_id",
-          as: "user",
-        },
-      },
-      {
-        $unwind: "$user",
-      },
-      {
-        $lookup: {
-          from: "projecttasks",
-          localField: "_id",
-          foreignField: "project",
-          as: "tasks",
-          pipeline: [
-            {
-              $lookup: {
-                from: "users",
-                localField: "assignedUsers",
-                foreignField: "_id",
-                as: "assignedUsers",
-              },
-            },
-            {
-              $lookup: {
-                from: "projecttaskmessages",
-                localField: "_id",
-                foreignField: "task",
-                as: "messages",
-                pipeline: [
-                  {
-                    $lookup: {
-                      from: "users",
-                      localField: "user",
-                      foreignField: "_id",
-                      as: "user",
-                    },
-                  },
-                  {
-                    $unwind: "$user",
-                  },
-                ],
-              },
-            },
-            {
-              $lookup: {
-                from: "projecttaskgroups",
-                localField: "group",
-                foreignField: "_id",
-                as: "group",
-              },
-            },
-            {
-              $unwind: "$group",
-            },
-          ],
-        },
-      },
-      {
-        $project: {
-          "user.password": 0,
-          "tasks.assignedUsers.password": 0,
-          "tasks.messages.user.password": 0,
-        },
-      },
-    ]);
-    if (!projects) throw Error("No such project");
-    res.status(200).json({ status: "success", data: projects[0] });
+    if (userIsAdmin) {
+      project = await adminFindOne(projectId);
+    } else {
+      project = await userFindOne(userId, projectId);
+    }
+    res.status(200).json({ status: "success", data: project });
   } catch (error) {
     res.status(500).json({ status: "failed", message: error.message });
   }
 };
 
-module.exports.adminFindAll = async (req, res) => {
+module.exports.findAll = async (req, res) => {
+  const { userId, userIsAdmin } = req;
   const { searchValue } = req.params;
+  let projects;
   try {
-    let projects = await Project.aggregate([
-      {
-        $lookup: {
-          from: "users",
-          localField: "user",
-          foreignField: "_id",
-          as: "user",
-        },
-      },
-      {
-        $unwind: "$user",
-      },
-      {
-        $project: {
-          "user.password": 0,
-        },
-      },
-    ]);
-    if (searchValue) {
-      projects = projects.filter((p) => {
-        p.title.toLowerCase().includes(searchValue.toLowerCase());
-      });
+    if (userIsAdmin) {
+      projects = await adminFindAll(searchValue);
+    } else {
+      projects = await userFindAll(userId, searchValue);
     }
     res.status(200).json({ status: "success", data: projects });
   } catch (error) {
@@ -117,163 +32,49 @@ module.exports.adminFindAll = async (req, res) => {
   }
 };
 
-// TODO: admin does not need to save new projects, maybe implement later if needed
-module.exports.adminSaveOne = async (req, res) => {};
+module.exports.saveOne = async (req, res) => {
+  const { userId, userIsAdmin } = req;
+  const { title } = req.body;
+  try {
+    // TODO: admin does not need to save new projects, maybe implement later if needed
+    const project = await userSaveOne(userId, title);
+    res.status(200).json({ status: "success", data: project });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
 
-module.exports.adminUpdateOne = async (req, res) => {
+module.exports.updateOne = async (req, res) => {
+  const { userId, userIsAdmin } = req;
   const { projectId } = req.params;
   const { title, status, user } = req.body;
+  let project;
   try {
-    if (!projectId || !mongoose.Types.ObjectId.isValid(projectId)) throw Error("Invalid project id");
     const projectObject = {};
     if (title) projectObject.title = title;
     if (status) projectObject.status = status;
     if (user) projectObject.user = user;
 
-    const project = await Project.findByIdAndUpdate(projectId, { ...projectObject }, { new: true });
-    if (!project) throw Error("Updating project failed");
-    res.status(200).json({ status: "success", data: project });
-  } catch (error) {
-    res.status(500).json({ status: "failed", message: error.message });
-  }
-};
-module.exports.adminDeleteOne = async (req, res) => {
-  const { projectId } = req.params;
-  try {
-    if (!projectId || !mongoose.Types.ObjectId.isValid(projectId)) throw Error("Invalid project id");
-    const project = await Project.findByIdAndDelete(projectId);
-    if (!project) throw Error("Deleting project failed");
-    res.status(200).json({ status: "success", data: project });
-  } catch (error) {
-    res.status(500).json({ status: "failed", message: error.message });
-  }
-};
-
-//controllers for specific user projects
-module.exports.findOneByUser = async (req, res) => {
-  const { userId, projectId } = req.params;
-  try {
-    if (!userId || !mongoose.Types.ObjectId.isValid(userId)) throw Error("Invalid user id");
-    if (!projectId || !mongoose.Types.ObjectId.isValid(projectId)) throw Error("Invalid project id");
-    const projects = await Project.aggregate([
-      { $match: { $and: [{ _id: new mongoose.Types.ObjectId(projectId) }, { user: new mongoose.Types.ObjectId(userId) }] } },
-      { $limit: 1 },
-      {
-        $lookup: {
-          from: "users",
-          localField: "user",
-          foreignField: "_id",
-          as: "user",
-        },
-      },
-      {
-        $unwind: "$user",
-      },
-      {
-        $lookup: {
-          from: "projecttasks",
-          localField: "_id",
-          foreignField: "project",
-          as: "tasks",
-          pipeline: [
-            {
-              $lookup: {
-                from: "users",
-                localField: "assignedUsers",
-                foreignField: "_id",
-                as: "assignedUsers",
-              },
-            },
-            {
-              $lookup: {
-                from: "projecttaskmessages",
-                localField: "_id",
-                foreignField: "task",
-                as: "messages",
-                pipeline: [
-                  {
-                    $lookup: {
-                      from: "users",
-                      localField: "user",
-                      foreignField: "_id",
-                      as: "user",
-                    },
-                  },
-                  {
-                    $unwind: "$user",
-                  },
-                ],
-              },
-            },
-            {
-              $lookup: {
-                from: "projecttaskgroups",
-                localField: "group",
-                foreignField: "_id",
-                as: "group",
-              },
-            },
-            {
-              $unwind: "$group",
-            },
-          ],
-        },
-      },
-      {
-        $project: {
-          "user.password": 0,
-          "tasks.assignedUsers.password": 0,
-          "tasks.messages.user.password": 0,
-        },
-      },
-    ]);
-    if (!projects) throw Error("Invalid project");
-    res.status(200).json({ status: "success", data: projects[0] });
-  } catch (error) {
-    res.status(500).json({ status: "failed", message: error.message });
-  }
-};
-
-module.exports.findAllByUser = async (req, res) => {
-  const { userId, searchValue } = req.params;
-  try {
-    if (!userId || !mongoose.Types.ObjectId.isValid(userId)) throw Error("Invalid user id");
-    let projects = await Project.find({ user: new mongoose.Types.ObjectId(userId) });
-    if (searchValue) {
-      projects = projects.filter((p) => {
-        p.title.toLowerCase().includes(searchValue.toLowerCase());
-      });
+    if (userIsAdmin) {
+      project = await adminUpdateOne(projectId, projectObject);
+    } else {
+      project = await userUpdateOne(userId, projectId, projectObject);
     }
-    res.status(200).json({ status: "success", data: projects });
-  } catch (error) {
-    res.status(200).json({ status: "failed", message: error.message });
-  }
-};
-module.exports.saveOneByUser = async (req, res) => {
-  const { userId } = req.params;
-  const { title } = req.body;
-  try {
-    if (!userId || !mongoose.Types.ObjectId.isValid(userId)) throw Error("Invalid user id");
-    if (!title) throw Error("All fields must be filled");
-    const project = await Project.create({ title: title, status: "New", user: userId });
-    if (!project) throw Error("Creating project failed");
     res.status(200).json({ status: "success", data: project });
   } catch (error) {
     res.status(500).json({ status: "failed", message: error.message });
   }
 };
-module.exports.updateOneByUser = async (req, res) => {
-  const { userId, projectId } = req.params;
-  const { title, status } = req.body;
+module.exports.deleteOne = async (req, res) => {
+  const { userIsAdmin } = req;
+  const { projectId } = req.params;
+  let project;
   try {
-    if (!userId || !mongoose.Types.ObjectId.isValid(userId)) throw Error("User id invalid");
-    if (!projectId || !mongoose.Types.ObjectId.isValid(projectId)) throw Error("Project id invalid");
-    const projectObject = {};
-    if (title) projectObject.title = title;
-    if (status) projectObject.status = status;
-
-    const project = await Project.findByIdAndUpdate(projectId, { ...projectObject }, { new: true });
-    if (!project) throw Error("Updating project failed");
+    if (userIsAdmin) {
+      project = await adminDeleteOne(projectId);
+    } else {
+      throw Error("Only admin can access this");
+    }
     res.status(200).json({ status: "success", data: project });
   } catch (error) {
     res.status(500).json({ status: "failed", message: error.message });
---
+++ services/project/project.js
@@ -0,0 +1,249 @@
+const Project = require("../../models/project/Project");
+const mongoose = require("mongoose");
+
+module.exports.adminFindOne = async (projectId) => {
+  if (!projectId || !mongoose.Types.ObjectId.isValid(projectId)) throw Error("Invalid project id");
+  const projects = await Project.aggregate([
+    { $match: { _id: new mongoose.Types.ObjectId(projectId) } },
+    { $limit: 1 },
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    {
+      $unwind: "$user",
+    },
+    {
+      $lookup: {
+        from: "projecttasks",
+        localField: "_id",
+        foreignField: "project",
+        as: "tasks",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "assignedUsers",
+              foreignField: "_id",
+              as: "assignedUsers",
+            },
+          },
+          {
+            $lookup: {
+              from: "projecttaskmessages",
+              localField: "_id",
+              foreignField: "task",
+              as: "messages",
+              pipeline: [
+                {
+                  $lookup: {
+                    from: "users",
+                    localField: "user",
+                    foreignField: "_id",
+                    as: "user",
+                  },
+                },
+                {
+                  $unwind: "$user",
+                },
+              ],
+            },
+          },
+          {
+            $lookup: {
+              from: "projecttaskgroups",
+              localField: "group",
+              foreignField: "_id",
+              as: "group",
+            },
+          },
+          {
+            $unwind: "$group",
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        "user.password": 0,
+        "tasks.assignedUsers.password": 0,
+        "tasks.messages.user.password": 0,
+      },
+    },
+  ]);
+  if (!projects[0]) throw Error("No such project");
+  return projects[0];
+};
+
+module.exports.adminFindAll = async (searchValue) => {
+  let projects = await Project.aggregate([
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    {
+      $unwind: "$user",
+    },
+    {
+      $project: {
+        "user.password": 0,
+      },
+    },
+  ]);
+  if (searchValue) {
+    projects = projects.filter((p) => {
+      p.title.toLowerCase().includes(searchValue.toLowerCase());
+    });
+  }
+  return projects;
+};
+
+module.exports.adminUpdateOne = async (projectId, projectObject) => {
+  if (!projectId || !mongoose.Types.ObjectId.isValid(projectId)) throw Error("Invalid project id");
+  const project = await Project.findByIdAndUpdate(projectId, { ...projectObject }, { new: true });
+  if (!project) throw Error("Updating project failed");
+  return project;
+};
+
+module.exports.adminDeleteOne = async (projectId) => {
+  if (!projectId || !mongoose.Types.ObjectId.isValid(projectId)) throw Error("Invalid project id");
+  const project = await Project.findByIdAndDelete(projectId);
+  if (!project) throw Error("Deleting project failed");
+  return project;
+};
+
+module.exports.userFindOne = async (userId, projectId) => {
+  if (!userId || !mongoose.Types.ObjectId.isValid(userId)) throw Error("Invalid user id");
+  if (!projectId || !mongoose.Types.ObjectId.isValid(projectId)) throw Error("Invalid project id");
+
+  const projects = await Project.aggregate([
+    { $match: { $and: [{ _id: new mongoose.Types.ObjectId(projectId) }, { user: new mongoose.Types.ObjectId(userId) }] } },
+    { $limit: 1 },
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    {
+      $unwind: "$user",
+    },
+    {
+      $lookup: {
+        from: "projecttasks",
+        localField: "_id",
+        foreignField: "project",
+        as: "tasks",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "assignedUsers",
+              foreignField: "_id",
+              as: "assignedUsers",
+            },
+          },
+          {
+            $lookup: {
+              from: "projecttaskmessages",
+              localField: "_id",
+              foreignField: "task",
+              as: "messages",
+              pipeline: [
+                {
+                  $lookup: {
+                    from: "users",
+                    localField: "user",
+                    foreignField: "_id",
+                    as: "user",
+                  },
+                },
+                {
+                  $unwind: "$user",
+                },
+              ],
+            },
+          },
+          {
+            $lookup: {
+              from: "projecttaskgroups",
+              localField: "group",
+              foreignField: "_id",
+              as: "group",
+            },
+          },
+          {
+            $unwind: "$group",
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        "user.password": 0,
+        "tasks.assignedUsers.password": 0,
+        "tasks.messages.user.password": 0,
+      },
+    },
+  ]);
+  if (!projects[0]) throw Error("No such project");
+  return projects[0];
+};
+
+module.exports.userFindAll = async (userId, searchValue) => {
+  if (!userId || !mongoose.Types.ObjectId.isValid(userId)) throw Error("Invalid user id");
+  let projects = await Project.aggregate([
+    { $match: { user: new mongoose.Types.ObjectId(userId) } },
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    {
+      $unwind: "$user",
+    },
+    {
+      $project: {
+        "user.password": 0,
+      },
+    },
+  ]);
+  if (searchValue) {
+    projects = projects.filter((p) => {
+      p.title.toLowerCase().includes(searchValue.toLowerCase());
+    });
+  }
+  return projects;
+};
+
+module.exports.userSaveOne = async (userId, title) => {
+  if (!userId || !mongoose.Types.ObjectId.isValid(userId)) throw Error("Invalid user id");
+  if (!title) throw Error("All fields must be filled");
+  const project = await Project.create({ title: title, status: "New", user: userId });
+  if (!project) throw Error("Creating project failed");
+  return project;
+};
+
+module.exports.userUpdateOne = async (userId, projectId, projectObject) => {
+  if (!userId || !mongoose.Types.ObjectId.isValid(userId)) throw Error("User id invalid");
+  if (!projectId || !mongoose.Types.ObjectId.isValid(projectId)) throw Error("Project id invalid");
+
+  const foundProject = await Project.find({ _id: new mongoose.Types.ObjectId(projectId), user: mongoose.Types.ObjectId(userId) });
+  if (!foundProject) throw Error("No such project");
+
+  const project = await Project.findByIdAndUpdate(projectId, { ...projectObject }, { new: true });
+  if (!project) throw Error("Updating project failed");
+};
