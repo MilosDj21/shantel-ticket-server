f3bae004ca0b5eb7d1242b523cc2e6d8e7e22f44
Milos <m.djuka@hotmail.com>
2024-01-24 15:27:23 +0100

    add client link controller

---
+++ controllers/project/clientLink.js
@@ -0,0 +1,7 @@
+module.exports.findOne = async (req, res) => {
+  res.status(200).json({ message: "find one client link" });
+};
+module.exports.findAll = async (req, res) => {};
+module.exports.saveOne = async (req, res) => {};
+module.exports.updateOne = async (req, res) => {};
+module.exports.deleteOne = async (req, res) => {};
