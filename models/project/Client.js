fc642b97f731309716c22fa94df95bc932a6db07
Milos <m.djuka@hotmail.com>
2024-01-24 14:14:17 +0100

    add client model

---
+++ models/project/Client.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const ClientSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Client = mongoose.model("Client", ClientSchema);
+
+module.exports = Client;
