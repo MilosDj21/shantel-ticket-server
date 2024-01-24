cb48848ea4184ce6891ced828a3466f632c76278
Milos <m.djuka@hotmail.com>
2024-01-24 14:14:48 +0100

    add post request model

---
+++ models/project/PostRequest.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const postRequestSchema = new mongoose.Schema(
+  {
+    website: {
+      type: String,
+      required: true,
+    },
+    websiteCategory: {
+      type: String,
+    },
+    postCategory: {
+      type: String,
+      required: true,
+    },
+    progressLevel: {
+      type: String,
+      required: true,
+    },
+    editor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    copywriter: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+    },
+    anchorKeyword: {
+      type: String,
+    },
+    clientPaidLink: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ClientLink",
+    },
+    textLink: {
+      type: String,
+    },
+    postLink: {
+      type: String,
+    },
+    urgencyLevel: {
+      type: String,
+    },
+    wordNum: {
+      type: Number,
+    },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+    },
+    clientHasText: {
+      type: Boolean,
+    },
+  },
+  { timestamps: true }
+);
+
+const PostRequest = mongoose.model("PostRequest", postRequestSchema);
+
+module.exports = PostRequest;
