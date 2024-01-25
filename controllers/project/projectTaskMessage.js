9a40f3e5e619dd61c15be95a307aeb384f29aab9
Milos <m.djuka@hotmail.com>
2024-01-25 12:26:20 +0100

    add task message controllers

--- controllers/project/projectTaskMessage.js
+++ controllers/project/projectTaskMessage.js
@@ -1,7 +1,70 @@
+const fs = require("fs");
+const path = require("path");
+const { findOne, findAll, saveOne, updateOne, deleteOne } = require("../../services/project/projectTaskMessage");
+
 module.exports.findOne = async (req, res) => {
-  res.status(200).json({ message: "find one task messages" });
+  const { messageId } = req.params;
+  try {
+    const message = await findOne(messageId);
+    res.status(200).json({ status: "success", data: message });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
 };
-module.exports.findAll = async (req, res) => {};
-module.exports.saveOne = async (req, res) => {};
-module.exports.updateOne = async (req, res) => {};
-module.exports.deleteOne = async (req, res) => {};
+
+module.exports.findAll = async (req, res) => {
+  try {
+    const messages = await findAll();
+    res.status(200).json({ status: "success", data: messages });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.saveOne = async (req, res) => {
+  const { userId } = req;
+  const { message, task } = req.body;
+  const messageImage = req.file;
+  try {
+    const image = messageImage ? messageImage.path : "";
+    const message = await saveOne(message, image, userId, task);
+    res.status(200).json({ status: "success", data: message });
+  } catch (error) {
+    if (messageImage) {
+      fs.unlink(path.join(__dirname, `../../${messageImage.path}`), (error) => {
+        if (error) console.log(error);
+      });
+    }
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.updateOne = async (req, res) => {
+  const { messageId } = req.params;
+  const { message } = req.body;
+  const messageImage = req.file;
+  try {
+    const messageObj = {};
+    if (message) messageObj.message = message;
+    if (messageImage) messageObj.image = messageImage.path;
+    const message = await updateOne(messageId, messageObj);
+    res.status(200).json({ status: "success", data: message });
+  } catch (error) {
+    if (messageImage) {
+      fs.unlink(path.join(__dirname, `../../${messageImage.path}`), (error) => {
+        if (error) console.log(error);
+      });
+    }
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.deleteOne = async (req, res) => {
+  const { messageId } = req.params;
+  try {
+    const message = await deleteOne(messageId);
+    res.status(200).json({ status: "success", data: message });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
