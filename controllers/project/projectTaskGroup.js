52e9e0cf65f50dcab00f14f7666df0c7f711bbbe
Milos <m.djuka@hotmail.com>
2024-01-25 10:43:55 +0100

    add task group controllers

--- controllers/project/projectTaskGroup.js
+++ controllers/project/projectTaskGroup.js
@@ -1,7 +1,51 @@
+const { findOne, findAll, saveOne, updateOne, deleteOne } = require("../../services/project/projectTaskGroup");
+
 module.exports.findOne = async (req, res) => {
-  res.status(200).json({ message: "find one task group" });
+  const { taskGroupId } = req.params;
+  try {
+    const group = await findOne(taskGroupId);
+    res.status(200).json({ status: "success", data: group });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
 };
-module.exports.findAll = async (req, res) => {};
-module.exports.saveOne = async (req, res) => {};
-module.exports.updateOne = async (req, res) => {};
-module.exports.deleteOne = async (req, res) => {};
+
+module.exports.findAll = async (req, res) => {
+  try {
+    const groups = await findAll();
+    res.status(200).json({ status: "success", data: groups });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.saveOne = async (req, res) => {
+  const { title } = req.body;
+  try {
+    const group = await saveOne(title);
+    res.status(200).json({ status: "success", data: group });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.updateOne = async (req, res) => {
+  const { taskGroupId } = req.params;
+  const { title } = req.body;
+  try {
+    const group = await updateOne(taskGroupId, title);
+    res.status(200).json({ status: "success", data: group });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.deleteOne = async (req, res) => {
+  const { taskGroupId } = req.params;
+  try {
+    const group = await deleteOne(taskGroupId);
+    res.status(200).json({ status: "success", data: group });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
