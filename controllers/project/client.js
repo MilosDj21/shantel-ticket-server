01d4fb502551ddb96254fb63f01b6059219cb793
Milos <m.djuka@hotmail.com>
2024-01-25 15:25:44 +0100

    add client controllers

--- controllers/project/client.js
+++ controllers/project/client.js
@@ -1,7 +1,55 @@
+const { findOne, findAll, saveOne, updateOne, deleteOne } = require("../../services/project/client");
+
 module.exports.findOne = async (req, res) => {
-  res.status(200).json({ message: "find one client" });
+  const { clientId } = req.params;
+  try {
+    const client = await findOne(clientId);
+    res.status(200).json({ status: "success", data: client });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
 };
-module.exports.findAll = async (req, res) => {};
-module.exports.saveOne = async (req, res) => {};
-module.exports.updateOne = async (req, res) => {};
-module.exports.deleteOne = async (req, res) => {};
+
+module.exports.findAll = async (req, res) => {
+  const { searchValue } = req.params;
+  try {
+    const clients = await findAll(searchValue);
+    res.status(200).json({ status: "success", data: clients });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.saveOne = async (req, res) => {
+  const { email, user } = req.body;
+  try {
+    const client = await saveOne(email, user);
+    res.status(200).json({ status: "success", data: client });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.updateOne = async (req, res) => {
+  const { clientId } = req.params;
+  const { email, user } = req.body;
+  try {
+    const clientObj = {};
+    if (email) clientObj.email = email;
+    if (user) clientObj.user = user;
+    const client = await updateOne(clientId, clientObj);
+    res.status(200).json({ status: "success", data: client });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.deleteOne = async (req, res) => {
+  const { clientId } = req.params;
+  try {
+    const client = await deleteOne(clientId);
+    res.status(200).json({ status: "success", data: client });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
