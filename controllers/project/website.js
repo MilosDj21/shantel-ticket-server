134c2928fde4e85ad3d59d620f50873344a9b27d
Milos <m.djuka@hotmail.com>
2024-01-26 15:22:28 +0100

    add website controllers

--- controllers/project/website.js
+++ controllers/project/website.js
@@ -1,7 +1,55 @@
+const { findOne, findAll, saveOne, updateOne, deleteOne } = require("../../services/project/website");
+
 module.exports.findOne = async (req, res) => {
-  res.status(200).json({ message: "find one website" });
+  const { websiteId } = req.params;
+  try {
+    const website = await findOne(websiteId);
+    res.status(200).json({ status: "success", data: website });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
 };
-module.exports.findAll = async (req, res) => {};
-module.exports.saveOne = async (req, res) => {};
-module.exports.updateOne = async (req, res) => {};
-module.exports.deleteOne = async (req, res) => {};
+
+module.exports.findAll = async (req, res) => {
+  const { searchValue } = req.params;
+  try {
+    const websites = await findAll(searchValue);
+    res.status(200).json({ status: "success", data: websites });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.saveOne = async (req, res) => {
+  const { url, category } = req.body;
+  try {
+    const website = await saveOne(url, category);
+    res.status(200).json({ status: "success", data: website });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.updateOne = async (req, res) => {
+  const { websiteId } = req.params;
+  const { url, category } = req.body;
+  try {
+    const websiteObj = {};
+    if (url) websiteObj.url = url;
+    if (category) websiteObj.category = category;
+    const website = await updateOne(websiteId, websiteObj);
+    res.status(200).json({ status: "success", data: website });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
+
+module.exports.deleteOne = async (req, res) => {
+  const { websiteId } = req.params;
+  try {
+    const website = await deleteOne(websiteId);
+    res.status(200).json({ status: "success", data: website });
+  } catch (error) {
+    res.status(500).json({ status: "failed", message: error.message });
+  }
+};
