f240828d8426a372e97429fed981788fef6ed481
Milos <m.djuka@hotmail.com>
2024-02-15 11:43:03 +0100

    move aggregate pipeline to separate function so i can use it also when saving new post

--- services/project/postRequest.js
+++ services/project/postRequest.js
@@ -3,6 +3,50 @@ const mongoose = require("mongoose");
 
 module.exports.findOne = async (postId) => {
   if (!postId || !mongoose.Types.ObjectId.isValid(postId)) throw Error("Invalid post id");
+  return aggregateFind(postId);
+};
+
+module.exports.findAll = async (searchValue) => {
+  let posts = await Post.find({});
+  if (!posts) throw Error("Invalid posts");
+  if (searchValue) {
+    posts = posts.filter((p) => {
+      return p.title.toLowerCase().includes(searchValue.toLowerCase());
+    });
+  }
+  return posts;
+};
+
+module.exports.saveOne = async (postObj) => {
+  const post = await Post.create({ ...postObj });
+  if (!post) throw Error("Creating post failed");
+  return aggregateFind(post._id);
+};
+
+module.exports.updateOne = async (userId, userRoles, postId, postObj) => {
+  if (!postId || !mongoose.Types.ObjectId.isValid(postId)) throw Error("Invalid post id");
+
+  // if editor has a post assigned to them that is waiting for publish, restrict assigning another post to same editor
+  if (userRoles.includes("Editor")) {
+    const posts = await Post.find({ progressLevel: "pendingPublish", editor: new mongoose.Types.ObjectId(userId) });
+    if (posts.length > 0) {
+      delete postObj.editor;
+    }
+  }
+
+  const post = await Post.findByIdAndUpdate(postId, { ...postObj }, { new: true });
+  if (!post) throw Error("Updating post failed");
+  return post;
+};
+
+module.exports.deleteOne = async (postId) => {
+  if (!postId || !mongoose.Types.ObjectId.isValid(postId)) throw Error("Invalid post id");
+  const post = await Post.findByIdAndDelete(postId);
+  if (!post) throw Error("Deleting post failed");
+  return post;
+};
+
+const aggregateFind = async (postId) => {
   const posts = await Post.aggregate([
     { $match: { _id: new mongoose.Types.ObjectId(postId) } },
     { $limit: 1 },
@@ -73,44 +117,4 @@ module.exports.findOne = async (postId) => {
   ]);
   if (!posts[0]) throw Error("No such post");
   return posts[0];
-};
-
-module.exports.findAll = async (searchValue) => {
-  let posts = await Post.find({});
-  if (!posts) throw Error("Invalid posts");
-  if (searchValue) {
-    posts = posts.filter((p) => {
-      return p.title.toLowerCase().includes(searchValue.toLowerCase());
-    });
-  }
-  return posts;
-};
-
-module.exports.saveOne = async (postObj) => {
-  const post = await Post.create({ ...postObj });
-  if (!post) throw Error("Creating post failed");
-  return post;
-};
-
-module.exports.updateOne = async (userId, userRoles, postId, postObj) => {
-  if (!postId || !mongoose.Types.ObjectId.isValid(postId)) throw Error("Invalid post id");
-
-  // if editor has a post assigned to them that is waiting for publish, restrict assigning another post to same editor
-  if (userRoles.includes("Editor")) {
-    const posts = await Post.find({ progressLevel: "pendingPublish", editor: new mongoose.Types.ObjectId(userId) });
-    if (posts.length > 0) {
-      delete postObj.editor;
-    }
-  }
-
-  const post = await Post.findByIdAndUpdate(postId, { ...postObj }, { new: true });
-  if (!post) throw Error("Updating post failed");
-  return post;
-};
-
-module.exports.deleteOne = async (postId) => {
-  if (!postId || !mongoose.Types.ObjectId.isValid(postId)) throw Error("Invalid post id");
-  const post = await Post.findByIdAndDelete(postId);
-  if (!post) throw Error("Deleting post failed");
-  return post;
 };
