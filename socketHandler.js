75d557c7012734ee04795afa5916b9a1fad9fb38
Milos <m.djuka@hotmail.com>
2024-03-05 15:54:48 +0100

    add event listeners for connecting socket id with user id, and for private messages

--- socketHandler.js
+++ socketHandler.js
@@ -1,7 +1,37 @@
-module.exports.socketHandler = (io) => {
-  const connectedUsers = [];
+const connectedUsers = [];
 
+module.exports.socketHandler = (io) => {
   io.on("connection", (socket) => {
-    console.log(socket);
+    console.log(socket.id);
+
+    //Send socket id to connected user
+    io.to(socket.id).emit("recieveSocketId", socket.id);
+
+    //Connect current user with socket user after connection
+    socket.on("connectWithDb", onConnectWithDb);
+
+    socket.on("privateMessage", (message) => {
+      console.log(message);
+      let recieveUserId = null;
+      for (const u of connectedUsers) {
+        if (u.dbUserId === message.to) {
+          recieveUserId = u.socketUserId;
+          break;
+        }
+      }
+      if (recieveUserId) io.to(recieveUserId).emit("privateMessage", message.data);
+    });
   });
+};
+
+const onConnectWithDb = (data) => {
+  for (const u of connectedUsers) {
+    if (u.dbUserId === data.dbUserId) {
+      u.socketUserId = data.socketUserId;
+      console.log(connectedUsers);
+      return;
+    }
+  }
+  connectedUsers.push(data);
+  console.log(connectedUsers);
 };
